0d1786050013fc63cd71b8ac4e0ac5f7abbea838
Parth <parth@parth-HP-450-Notebook-PC.(none)>
2013-12-18 16:22:18 +0530

    fix the bug in mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,7 @@ Routes::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   config.active_record.auto_explain_threshold_in_seconds = 0.5
-
+  config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
   # Do not compress assets
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,9 @@
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {  
-	:address              => "smtp.outlook.com",  
-	:port                 => 587,  
-	:domain               => "outlook.com",  
-	:user_name            => ENV['Email'],  
-	:password             => ENV['PASSWORD'],  
-	:authentication       => "plain",  
-	:enable_starttls_auto => true  
-}
+ActionMailer::Base.smtp_settings = { 
+   :address              => "smtp.live.com",
+   :port                 => 587,
+   :enable_starttls_auto => true,
+   :user_name            => ENV['EMAIL'],  
+   :password             => ENV['PASSWORD'],
+   :domain               => 'outlook.com',
+   :authentication       => 'plain'
+ }
